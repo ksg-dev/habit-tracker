9394b72f9c6a5ccbe49d77d9cc0108b9391459e5
ksg-dev <ksg.dev.data@gmail.com>
2024-06-18 15:06:54 -0500

    add data to graph1

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ user_parameters = {
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
-
+# create user
 # response = requests.post(url=pixela_endpoint, json=user_parameters)
 # print(response.text)
 
+# create graph
 graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
 # View Graph: https://pixe.la/v1/users/ksg-dev/graphs/graph1.html
@@ -31,5 +32,16 @@ headers = {
     "X-USER-TOKEN": TOKEN,
 }
 
-response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+# print(response.text)
+
+# add value to graph
+add_data_endpoint = f"{graph_endpoint}/graph1"
+
+data_config = {
+    "date": "20240618",
+    "quantity": "5",
+}
+
+response = requests.post(url=add_data_endpoint, json=data_config, headers=headers)
 print(response.text)
