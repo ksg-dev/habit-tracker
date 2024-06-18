ba65ae29cf107b735fabbbc05aec329d658aeb6e
ksg-dev <ksg.dev.data@gmail.com>
2024-06-18 15:00:57 -0500

    graph1 base

--- main.py
+++ main.py
@@ -1,1 +1,35 @@
 import requests
+
+USERNAME = "ksg-dev"
+TOKEN = "aiuehfo82398hfalsej"
+
+pixela_endpoint = "https://pixe.la/v1/users"
+
+user_parameters = {
+    "token": TOKEN,
+    "username": USERNAME,
+    "agreeTermsOfService": "yes",
+    "notMinor": "yes",
+}
+
+# response = requests.post(url=pixela_endpoint, json=user_parameters)
+# print(response.text)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+# View Graph: https://pixe.la/v1/users/ksg-dev/graphs/graph1.html
+
+graph_config = {
+    "id": "graph1",
+    "name": "Cycling Graph",
+    "unit": "Km",
+    "type": "float",
+    "color": "ajisai"
+}
+
+headers = {
+    "X-USER-TOKEN": TOKEN,
+}
+
+response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
+print(response.text)
